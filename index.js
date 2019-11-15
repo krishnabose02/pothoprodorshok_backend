9bdccdd4df0faf710321006193445ea053e82ec8
Ankan <ankantech96@gmail.com>
2019-11-15 14:04:05 +0530

    login api added

--- index.js
+++ index.js
@@ -29,29 +29,58 @@ client.connect(err => {
 
   app.post('/posthazard', (req, res) => {
     db.collection("locations").insertOne(req.body).then((element) => {
-      res.send(element);
+      res.status(200).send({
+        "message": "Posted"
+      })
+    }).catch((error) => {
+      res.status(404).send(error)
     })
   })
 
   app.post('/getnearby', (req, res) => {
-    db.collection("locations").find ({
+    db.collection("locations").find({
       location: {
         $near: {
           $geometry: {
-            type: "Point" ,
+            type: "Point",
             coordinates: req.body.coordinates
           },
           $maxDistance: req.body.radius,
           $minDistance: 0
         }
       }
-    }).toArray().then((element)=>{
+    }).toArray().then((element) => {
       res.send(element);
+    })
+  })
+  app.post('/login', (req, res) => {
+    db.collection("users").find(req.body).toArray().then((element) => {
+      if(element.length!=0)
+      res.send(element);
+      else
+      res.status(404).send(
+        {
+          "message":"Not Found"
+        }
+      )
+    }).catch((error) => {
+      res.status(400).send(error);
+
+    });
+
+  })
+  app.post('/postuser', (req, res) => {
+
+    db.collection("users").insertOne(req.body).then((element) => {
+      res.send(element);
+
+    }).catch((error) => {
+      res.send(error);
     })
   })
 
   app.get('/', (req, res) => res.sendfile('public/index.html'))
 
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 });
